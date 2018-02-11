8f508e9af2857c70641e1892a6449dea86e2b4fc
nmsz97 <syed.jafri2@ucalgary.ca>
2018-02-11 16:28:23 -0700

    Set up database

--- src/database.js
+++ src/database.js
@@ -2,11 +2,10 @@ const knex = require('knex');
 
 module.exports = function(app) {
   // Database connector
-  console.log(app.get("database"));
   const db = knex(app.get("database"));
 
-  // Create the Patients
-  db.schema.createTable('patients', table => {
+  // Create the Physicians
+  db.schema.createTable('physicians', table => {
     table.increments('id');
     table.string('username');
 
--- src/services/physicians/physicians.service.js
+++ src/services/physicians/physicians.service.js
@@ -4,6 +4,7 @@ const createService = require('feathers-knex');
 
 module.exports = function (app) {
   let db = app.get('db')
+  
   app.use('/physicians', createService({
     Model: db,
     name: 'physicians'
