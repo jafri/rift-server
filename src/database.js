1d50025cc37227f0c950ab18e6ada09c486c9bcc
Mason Brothers <brothers.mason@gmail.com>
2018-02-11 17:09:28 -0700

    fixed

--- src/database.js
+++ src/database.js
@@ -7,6 +7,37 @@ module.exports = function(app) {
   // Creat uuid extension
   // knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
 
+  // Create the Patients Table
+  db.schema.withSchema('ehr').hasTable('patients').then(function(exists) {
+    if (!exists) {
+      createTable('patients', table => {
+        table.increments('id').primary();
+        table.string('first_name', 100).notNullable();
+        table.string('last_name', 100).notNullable();
+        table.string('middle_name', 100);
+        table.string('personal_health_care_number', 100);
+        table.date('date_of_birth');
+        table.boolean('research_consent').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+      });
+    }
+  });
+
+  // Create the Patient Records Table
+  db.schema.withSchema('ehr').hasTable('patients').then(function(exists) {
+    if (!exists) {
+      db.schema.withSchema('ehr').createTable('patient_reports', table => {
+        table.increments('id').primary();
+        table.string('report_type');
+        table.string('patient_id').references('patient_id').inTable('patients');
+        table.string('physician_id').references('physician_id').inTable('physicians');
+        table.jsonb('form_json');
+        table.string('password');
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+      });
+    }
+  });
+
   knex.schema.withSchema('ehr').hasTable('physicians').then(function(exists) {
     if (!exists) {
       return knex.schema.withSchema('ehr').createTable('physicians', function(table) {
@@ -30,6 +61,8 @@ module.exports = function(app) {
       });
     }
   });
+
+
 
   app.set('db', db);
 };
