d68aabadc17d5bd933dfb3eb3d1b3cc38d309e87
nmsz97 <syed.jafri2@ucalgary.ca>
2018-02-11 17:05:05 -0700

    Add patient tables

--- src/database.js
+++ src/database.js
@@ -4,12 +4,31 @@ module.exports = function(app) {
   // Database connector
   const db = knex(app.get("database"));
 
-  // Create the Physicians
-  db.schema.createTable('physicians', table => {
-    table.increments('id');
-    table.string('username');
+  // Creat uuid extension
+  knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
 
-    table.string('password');
+  knex.schema.withSchema('ehr').hasTable('physicians').then(function(exists) {
+    if (!exists) {
+      return knex.schema.withSchema('ehr').createTable('physicians', function(table) {
+        table.uuid('physician_id').defaultTo(knex.raw('uuid_generate_v1mc()')).primary();
+        table.string('first_name', 100);
+        table.string('last_name', 100);
+        table.string('middle_name', 100);
+        table.string('email');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+      });
+    }
+  });
+
+  knex.schema.withSchema('ehr').hasTable('physician_logins').then(function(exists) {
+    if (!exists) {
+      return knex.schema.withSchema('ehr').createTable('physician_logins', function(table) {
+        t.increments('id').primary();
+        t.string('first_name', 100);
+        t.string('last_name', 100);
+        t.text('bio');
+      });
+    }
   });
 
   app.set('db', db);
